77915214269f8f6a15cdd8afd932812138d82e47
Piotr Andrzejak <pandrzejak93@gmail.com>
2019-11-11 13:43:39 +0100

    Piotr-Andrzejak-Test

--- spring-boot-backend/src/main/java/com/instantor/dap/springbootbackend/controller/StarWarsCharacterController.java
+++ spring-boot-backend/src/main/java/com/instantor/dap/springbootbackend/controller/StarWarsCharacterController.java
@@ -1,17 +1,25 @@
 package com.instantor.dap.springbootbackend.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-@RequestMapping("/api/v1/sw")
+@RequestMapping("/api/v1/sw/character")
 public class StarWarsCharacterController {
 
     /**
      * @return character from Star Wars
      */
-    @GetMapping("/character")
+    @GetMapping("/introduction")
     public String getCharacter() {
-        return "{}";
+        return StartWarsCharacterConst.vaderWelcome;
+    }
+
+    @GetMapping("/description")
+    public String getCharacterDescription() {
+        return StartWarsCharacterConst.vaderDescription;
     }
 }
--- spring-boot-backend/src/main/java/com/instantor/dap/springbootbackend/controller/StartWarsCharacterConst.java
+++ spring-boot-backend/src/main/java/com/instantor/dap/springbootbackend/controller/StartWarsCharacterConst.java
@@ -1,4 +1,21 @@
 package com.instantor.dap.springbootbackend.controller;
 
 public class StartWarsCharacterConst {
+
+    public static final String vaderWelcome = "This is Lord Vader." +
+            " \n " +
+            "Click on the below buttons to learn more before your mission :)";
+
+    public static final String vaderDescription = "Behold one of the darkest character in the Universe soldier." +
+            " \n " +
+            "This is a Black Start of the Empire. Sith Lord, Darth Vader" +
+            " \n " +
+            "Previously known as lovely boy Anakin Skywalker, today he is embodied of the evil and betray" +
+            " \n " +
+            "Your mission won't be easy, soldier. You will risk yours and ours lifes" +
+            " \n " +
+            "We got one more advice for you. Do not smoke some much..." +
+            " \n " +
+            "Cigarettes make you to start breath like him!";
 }
+
